c8619f9f2ceddd3eebdca6b4ab72246fc947ac37
driev <4562759+driev@users.noreply.github.com>
2023-05-09 19:24:00 +0100

    keymaps and rust_analyzer

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,15 @@
+return {
+
+  -- add rust_analyzer to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        rust_analyzer = {},
+      },
+    },
+  },
+}
