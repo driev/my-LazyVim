d250c95df53d6ed38e5f762561f86751289b70ac
Drievs <4562759+driev@users.noreply.github.com>
2024-03-23 20:51:38 +0000

    adding colourscheme

---
+++ lua/plugins/colourscheme.lua
@@ -0,0 +1,26 @@
+return {
+  -- add gruvbox
+  -- { "ellisonleao/gruvbox.nvim" },
+  --
+  -- configure lazyvim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvbox",
+  --   },
+  -- },
+  { "EdenEast/nightfox.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "carbonfox",
+    },
+  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "slate",
+  --   },
+  -- },
+}
